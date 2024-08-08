c6c85c470158e9adce15d4557cc1a62b08a63793
Paco Mastermind <paco@mastermind.ac>
2024-08-08 17:00:02 +0200

    Cuarata prueba

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import random
-cartas="1,2,3"
+cartas=[1,2,3]
 inicio=1
 if inicio==1:
     print("¡Hola mundo!")
     print(cartas)
+    for num in cartas:
+        print(num)
+        while num<3:
+            print("prueba")
